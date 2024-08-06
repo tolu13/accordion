23cac8a242bcbdbb7eb028e2f7d4a4aed3796d58
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-08-06 11:00:25 +0100

    getting busy

---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id : '1',
+        question: 'what are accordion components',
+        answer:
+            'Accordion components are user interface elements  to organize content and allow users to expand or collapse sections.',
+    },
+    {
+        id : '2',
+        question: 'what are they used for ?',
+        answer:
+            'They are commonly employed in variation contexts, including FAQs, swtting panles, navigation.',
+    },
+    {
+        id : '3',
+        question: 'Accordion a s a musical instrument',
+        answer:
+            'The accordion is a musical instrument with a keyboard and bellos',
+    },
+    {
+        id : '4',
+        question: ' can i create an accordion component with a different framework',
+        answer:
+            'Yes of course, it is very possible to create an accordion componet with a different framework',
+    },
+];
+
+export default data;
---
+++ src/components/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import data from "./data";
+import '../styles.css';
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    return (
+     <div className="wrapper">
+        <button>Enable Multi Selection</button>
+        <div className="accordion">
+            {data && data.length > 0 ? (
+                data.map((dataItem) => (
+                    <div className="item">
+                      <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                      </div>
+                      {
+                        selected === dataItem.id ?
+                        <div className="content">{dataItem.answer}</div>
+                        : null
+                      }
+                    </div>
+                ))
+            ) : (
+                <div>No data found !</div> 
+            )}
+        </div>
+     </div>
+    );
+}
